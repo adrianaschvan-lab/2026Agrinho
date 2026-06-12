6071302a521bcd21d2ab585add7fef736d482f62
adrianaschvan-lab <adriana.schvan@escola.pr.gov.br>
2026-06-11 22:14:05 -0300

    Add function to display sustainability message JAVA

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+    document.getElementById("mensagem").innerHTML =
+        "A sustentabilidade garante a produção de alimentos sem comprometer os recursos naturais das futuras gerações.";
+}
